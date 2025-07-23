989537a2a81a7784cba94b347971ced0135c7b91
Mohamed Amged <mohamedamged55555@gmail.com>
2025-07-23 20:36:58 +0300

    try fix

--- src/app/[country]/page.tsx
+++ src/app/[country]/page.tsx
@@ -1,10 +1,6 @@
 import CountryPageClient from "@/components/CountryPageClient"
 
-interface PageProps {
-  params: {
-    country: string
-  }
-}
+
 
 export default function Page({ params }: { params: { country: string } }) {
   return <CountryPageClient country={params.country} />

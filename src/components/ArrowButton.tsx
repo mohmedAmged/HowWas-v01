bfbd30345821a4f6a4cfddde06d2f92798029def
Mohamed Amged <mohamedamged55555@gmail.com>
2025-07-24 17:46:49 +0300

    fix some bugs

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -1,11 +1,13 @@
 // components/ArrowButton.tsx
 'use client'
+import { ComponentProps } from 'react'
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi'
+type IconProps = ComponentProps<'svg'>
 
-export function ArrowLeft(props) {
+export function ArrowLeft(props: IconProps) {
   return <FiArrowLeft size={20} color="white" {...props} />
 }
 
-export function ArrowRight(props) {
+export function ArrowRight(props: IconProps) {
   return <FiArrowRight size={20} color="white" {...props} />
 }
